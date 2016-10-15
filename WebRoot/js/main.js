ef867518b30d996ab538dde4746aa5185f49fa3b
muzi <623337308@qq.com>
2016-10-16 00:07:58 +0800

    add css js

---
+++ WebRoot/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+   $(".owl-carousel").owlCarousel({
+       items:1,
+       loop:true,
+       autoplay:true,
+       autoplayTimeout:3000,
+       autoplayHoverPause:true
+   });
+
+    // $('.footer').on('scrollSpy:enter', function() {
+    //
+    //    $('.footer').addClass('animated  bounceInRight');
+    // });
+    //
+    // // $('.footer').on('scrollSpy:exit', function() {
+    // //     alert("qwert")
+    // //     $('.product').addClass('animated  bounceInRight');
+    // // });
+    //
+    // $('.footer').scrollSpy();
+});
