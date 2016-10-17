e06dfde0dba96d96ed159f4e7b2ba7bc73d473df
14594007360 <1459407360@qq.com>
2016-10-17 16:04:53 +0000

    login-test

--- src/com/ysk/controller/UserAction.java
+++ src/com/ysk/controller/UserAction.java
@@ -7,17 +7,28 @@ import org.springframework.stereotype.Controller;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.ysk.service.UserService;
+import com.ysk.vo.User;
 @Controller
 @Scope("prototype")
 public class UserAction extends ActionSupport {
 	@Resource
 	private UserService userService;
 	
+	private User user;
+	
 	public String login(){
 		
-		
+		int result = userService.login(user);
 		
 		return SUCCESS;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 	
 }
